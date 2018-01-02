cbfb53c90d0d2ed99af2adb841415e753993f933
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-01-02 00:49:35 -0200

    Add files via upload

--- helpers-ui.php
+++ helpers-ui.php
@@ -54,6 +54,104 @@ class Ui
 	}
 
 
+
+	static function upload($attrs=null, $value=null)
+	{
+		$attrs = self::_attrs($attrs, array(
+			'type' => 'text',
+			'class' => 'form-control ui-upload-input',
+			'value' => $value,
+		));
+
+		$id = uniqid('ui-upload-');
+
+		?>
+		<div class="input-group ui-upload" id="<?php echo $id; ?>">
+			<input <?php echo $attrs; ?>>
+			<input type="file" class="ui-upload-file" style="display:none;">
+			<div class="input-group-btn">
+				<button type="button" class="btn btn-default ui-upload-btn">
+					<i class="fa fa-fw fa-upload"></i>
+				</button>
+			</div>
+			<div class="ui-upload-progress" style=""></div>
+		</div>
+		<script>
+		jQuery(document).ready(function($) {
+			var $parent = $("#<?php echo $id; ?>");
+			var $input = $parent.find(".ui-upload-input");
+			var $file = $parent.find(".ui-upload-file");
+			var $btn = $parent.find(".ui-upload-btn");
+			var $progress = $parent.find(".ui-upload-progress");
+
+			$btn.on("click", function() {
+				$file.click();
+			});
+
+			$file.on("change", function() {
+				if (this.files[0]) {
+					var formdata = new FormData();
+					formdata.append('upload', this.files[0]);
+					jQuery.ajax({
+						url: "<?php echo admin_url('admin-ajax.php?helper_upload=true'); ?>",
+						type: "post",
+						dataType: "json",
+						contentType: false,
+						processData: false,
+						data: formdata,
+						xhr: function() {
+							var xhr = $.ajaxSettings.xhr();
+							if(xhr.upload) {
+								xhr.upload.addEventListener('progress', function(e) {
+									if(e.lengthComputable) {
+										var percent = (e.loaded * 100)/e.total;
+										$progress.css({width:percent+'%'});
+									}
+								}, false);
+							}
+							return xhr;
+						},
+						success: function(response) {
+							$progress.css({width:0});
+							if (response.url) {
+								$input.val(response.url);
+							}
+						},
+						error: function(response) {
+							$progress.css({width:0});
+						},
+					});
+				}
+			});
+
+
+			$parent.on("drag dragstart dragover dragenter", function(ev) {
+				ev.preventDefault();
+				ev.stopPropagation();
+				$(this).addClass("ui-upload-dnd");
+			});
+
+			$parent.on("dragend dragleave drop", function(ev) {
+				ev.preventDefault();
+				ev.stopPropagation();
+				$(this).removeClass("ui-upload-dnd");
+				$file[0].files = ev.originalEvent.dataTransfer.files;
+				$file.trigger("change");
+			});
+		});
+		</script>
+		<style>
+		.ui-upload {position:relative;}
+		.ui-upload * {transition:all 100ms ease;}
+		.ui-upload-dnd {box-shadow:0px 0px 0px 15px rgba(36, 164, 193, 0.78), 0px 0px 0px 5px rgba(36, 164, 193, 0.78)}
+		.ui-upload-dnd * {background:rgba(36, 164, 193, 0.78) !important; border:none !important; box-shadow:none !important; border-radius:0}
+		.ui-upload-progress {position:absolute; top:100%; left:0px; display:block; display:block; border:solid 1px green}
+		</style>
+		<?php
+	}
+
+
+
 	static function posts($name, $value=null)
 	{
 		
@@ -127,3 +225,36 @@ class Ui
 	<?php }
 
 }
+
+
+
+add_action('init', function() {
+	
+	// UI Ajax upload
+	if (isset($_REQUEST['helper_upload']) AND !empty($_REQUEST['helper_upload'])) {
+
+		$return = array();
+
+		$return['url'] = false;
+		$return['path'] = false;
+		$return['upload'] = false;
+
+		if ($_FILES['upload']['tmp_name']) {
+			
+			$wp_upload_dir = wp_upload_dir();
+			$return['path'] = isset($_REQUEST['path'])? trim($_REQUEST['path'], '/'): null;
+			$return['path'] = "{$wp_upload_dir['basedir']}/{$return['path']}";
+
+			$_FILES['upload']['name'] = preg_replace('/[^a-zA-Z0-9-_.]/', '', $_FILES['upload']['name']);
+			$return['upload'] = $_FILES['upload'];
+			if (move_uploaded_file($_FILES['upload']['tmp_name'], "{$return['path']}/{$_FILES['upload']['name']}")) {
+				$return['url'] = "{$wp_upload_dir['baseurl']}/{$_FILES['upload']['name']}";
+			}
+		}
+
+		echo json_encode($return, JSON_PRETTY_PRINT); die;
+	}
+
+});
+
+
