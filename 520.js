9f88a457cdeeea262934b7d485a22b99b78c485f
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-01-08 15:22:17 -0200

    Add files via upload

---
+++ 520.js
@@ -0,0 +1,53 @@
+var $=jQuery;
+
+
+var script = document.createElement("script");
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js";
+document.head.appendChild(script);
+
+
+jQuery.component = function(tagname, settings) {
+	var proto = Object.create(HTMLElement.prototype);
+	return document.registerElement(tagname, {prototype:proto});
+};
+
+
+function $_(attr, files, call) {
+	var $els = $("["+attr+"]");
+	call = typeof call=="function"? call: function() {};
+	if ($els.length) {
+		head.load(files, function() {
+			$els.each(function() {
+				var opts = $(this).attr("data-table")||"{}";
+				try { eval('opts='+opts); } catch(e) { opts={}; }
+				call.call(this, opts);
+			});
+		});
+	}
+}
+
+var cdzInit=function() {};
+
+$.getScript("https://cdnjs.cloudflare.com/ajax/libs/headjs/1.0.3/head.load.min.js", function() {
+	var cdzInit = function() {
+
+		// Data mask
+		$_("data-mask", ["https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.min.js"]);
+		
+		// Data table
+		$_("data-table", [
+			"https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/jquery.dataTables.min.js",
+			"https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap.min.js",
+			"https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/css/dataTables.bootstrap.min.css",
+		], function(opts) {
+			opts = $.extend({
+				language: {
+					url: "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json",
+				},
+			}, opts);
+			$(this).DataTable(opts);
+		});
+
+	};
+	cdzInit.call();
+});
