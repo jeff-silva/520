1072064ecbd8bc48138a2d7cfe951a2170af0047
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-03-04 20:30:40 -0300

    Update init.php

--- Posttypes/init.php
+++ Posttypes/init.php
@@ -1,10 +1,16 @@
 <?php
 
 
+// Register posttypes
 add_action('init', function() {
 	$posttypes = new \Cdz\Posttypes\Posttypes();
-	$posttypes->register();
+	$posttypes = $posttypes->posttypeSearch();
+	foreach($posttypes as $posttype) {
+		//dd($posttype); die;
+		register_post_type($posttype['posttype_slug'], $posttype['posttype_data']);
+	}
 });
+
 
 add_action('520-settings', function() {
 	cdz_tab('Post types', function() {
