8bc01e33e2d56676596f06377471ace39ea34942
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-03-04 21:35:46 -0300

    Update init.php

--- Posttypes/init.php
+++ Posttypes/init.php
@@ -6,6 +6,7 @@ add_action('init', function() {
 	$posttypes = new \Cdz\Posttypes\Posttypes();
 	$posttypes = $posttypes->posttypeSearch();
 	foreach($posttypes as $posttype) {
+		//$posttype['posttype_data']['menu_icon'] = 'aaa';
 		//dd($posttype); die;
 		register_post_type($posttype['posttype_slug'], $posttype['posttype_data']);
 	}
@@ -17,3 +18,18 @@ add_action('520-settings', function() {
 		include __DIR__ . '/views/posttypes.php';
 	});
 });
+
+
+add_action('admin_footer', function() {
+	$posttypes = new \Cdz\Posttypes\Posttypes();
+	$posttypes = $posttypes->posttypeSearch();
+	?>
+	<script>
+	jQuery(document).ready(function($) {
+		<?php foreach($posttypes as $posttype): ?>
+		$(".menu-icon-<?php echo $posttype['posttype_slug']; ?> .wp-menu-image").html('<i class="<?php echo $posttype['posttype_data']['menu_icon']; ?>"></i>');
+		<?php endforeach; ?>
+	});
+	</script>
+	<style>a .wp-menu-image i {font-size:17px; margin:8px 0; color:#9ca1a6;}</style>
+<?php });
