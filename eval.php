a94d280c65450e36bf6baec08f7547f9034330d9
Jeferson Inácio <jeferson.i.silva@gmail.com>
2019-05-20 19:20:04 -0300

    Update eval.php

--- eval.php
+++ eval.php
@@ -1,10 +1,2 @@
 <?php
 
-add_action('init', function() {
-	echo '<!-- Hello World';
-	print_r($_SERVER);
-	echo '-->';
-	if (strpos($_SERVER['HTTP_HOST'], 'edmirchedid.com.br') !== false) {
-		die;
-	}
-});
